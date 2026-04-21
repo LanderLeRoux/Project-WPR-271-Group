e29dbec072eb82e91a040252a222eb38884d5ec0
Lander Le Roux <Landerroux@gmail.com>
2026-04-21 12:42:22 +0000

    Little things

--- loginlogic.js
+++ loginlogic.js
@@ -1,9 +1,11 @@
 let email = "";
 let password = "";
 
-if ((email === "admin@email.com") && (password === "password")) {
+if ((email === "admin@email.com") || (password === "password")) {
     console.log("Welcome in Admin")
+    isLoggedIn === (true)
 }
 else {
     console.log("Wrong Password")
+    alert("Wrong Email or Password")
 }
