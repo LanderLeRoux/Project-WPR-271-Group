08720368a75077ac4352fcb88962d5cb51e69f4f
Lander Le Roux <Landerroux@gmail.com>
2026-04-21 12:32:10 +0000

    Login Javascript

---
+++ loginlogic.js
@@ -0,0 +1,9 @@
+let email = "";
+let password = "";
+
+if ((email === "admin@email.com") && (password === "password")) {
+    console.log("Welcome in Admin")
+}
+else {
+    console.log("Wrong Password")
+}
